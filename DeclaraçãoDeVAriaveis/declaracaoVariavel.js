c8615e708611dbf15daed38e17f3b2c0eb407de2
João Aluno <lolbr313@gmail.com>
2025-05-11 08:43:43 -0300

    declaracaoVariavel Joao L.js

--- DeclaraçãoDeVAriaveis/declaracaoVariavel.js
+++ DeclaraçãoDeVAriaveis/declaracaoVariavel.js
@@ -1,15 +1,19 @@
 //Exercícios de declaração de variável (JavaScript)
 
 //1. Declare uma variável para armazenar seu nome:
-
+char João Lucas
 
 //2. Declare uma constante para armazenar sua idade:
-
+cons 20
 
 //3. Declare uma variável para armazenar seu peso e depois altere o valor:
-
+char 20
+if 19 > 20
+then 19 true
 
 //4. Declare uma variável para armazenar o nome de um país:
-
+let Brasil
 
 //5. Declare uma variável(lógica) para armazenar se você gosta de programação (true/false):
+
+char gostaDeProgramação true
