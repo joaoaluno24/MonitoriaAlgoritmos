6051af6a05bdce1f489e26d6579f6d71bf01623c
João Aluno <lolbr313@gmail.com>
2025-05-27 11:30:59 -0300

    Update exerciciosCondicional.js

--- EstruturasCondicionaisSimples/exerciciosCondicional.js
+++ EstruturasCondicionaisSimples/exerciciosCondicional.js
@@ -2,15 +2,24 @@
 
 //1. Verifique se um número é maior que 10
 
+let, > = "é maior que"
 
+console.log (10 "é maior que" 9?)
+console.log (true || false)
+  
 
 //2. Verifique se uma pessoa pode votar (idade mínima 18)
 
+let 18 = total
 
+  if(total <= total ; true; total++)
+  console.log( "Tem" idade "anos. Pode votar?")
+console.log(true || false)
 
 //3. Verifique se o nome tem mais de 5 caracteres
+let João = x
 
-
+Console.log("João tem" João <= 5 = true  "letras")
 
 //4. Verifique se um número é par ou ímpar
 
