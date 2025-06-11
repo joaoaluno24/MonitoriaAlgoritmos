c71ff76bfb268dfc3e4c1880504a8e30e9c058e6
João Aluno <lolbr313@gmail.com>
2025-06-11 14:04:39 -0300

    Update exerciciosOperadores.js

--- operadoresLogicos/exerciciosOperadores.js
+++ operadoresLogicos/exerciciosOperadores.js
@@ -11,12 +11,20 @@ console.log("Menos de" 15 "Anos")
 //Saída:
 Pessoas maiores de 20 anos é permitido
 Menos de 15 anos
-/*
+
 //2. Acesso ao sistema
 //Crie um código que verifique se o usuário pode acessar o sistema. Ele só pode entrar se tiver login e senha preenchidos.
 
+let N = João
+let S = 8
 
+console.log("Site de Estudos");
+console.log("Nome:");
+console.log(["digite aqui"]if(N; N <= 4; true);
+console.log("Senha:");
+console.log(["digite aqui"] if(S)
 
+            
 //3. Escolha de plano
 //Verifique se o usuário tem acesso a um plano VIP ou Premium com base nas variáveis  e . Se pelo menos uma dessas condições for verdadeira, ele tem acesso especial.
 
